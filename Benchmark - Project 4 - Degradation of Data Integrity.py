6b1e6b93d7527e56b046d4184d33a3689181abdc
BenC2026 <BCroyle1@my.gcu.edu>
2024-11-03 00:42:25 -0700

    Update Benchmark - Project 4 - Degradation of Data Integrity.py

--- Benchmark - Project 4 - Degradation of Data Integrity.py
+++ Benchmark - Project 4 - Degradation of Data Integrity.py
@@ -29,3 +29,33 @@ plt.title('Plot of $x(t)=[e^{-t/20}, -e^{-t/20}]$')
 plt.legend()
 plt.grid(True)
 plt.show()
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.linalg import expm
+
+# Define the matrix A
+A = np.array([[-1/50, 3/100], [1/50, -3/100]])
+
+# Define the initial conditions
+x0 = np.array([1, -1])
+
+# Define time points
+t = np.linspace(0, 100, 400)
+
+# Calculate e^At for each time point
+x = np.array([expm(A * ti).dot(x0) for ti in t])
+
+# Extract the solutions x1(t) and x2(t)
+x1 = x[:, 0]
+x2 = x[:, 1]
+
+# Plotting the solutions
+plt.figure()
+plt.plot(t, x1, label='$x_1(t)$')
+plt.plot(t, x2, label='$x_2(t)$')
+plt.xlabel('t')
+plt.ylabel('$x(t)$')
+plt.title('Solution of the System of Differential Equations')
+plt.legend()
+plt.grid(True)
